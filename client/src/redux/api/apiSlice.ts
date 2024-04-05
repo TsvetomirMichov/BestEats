265256706945ff520f74572445b1c16b6013ca5d
TsvetomirMichov <94786397+TsvetomirMichov@users.noreply.github.com>
2024-04-05 16:27:29 +0300

    Update apiSlice.ts

--- client/src/redux/api/apiSlice.ts
+++ client/src/redux/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { RootState } from '../store'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:1337',
+    baseUrl: 'besteats-production.up.railway.app',
     credentials: 'include',
     prepareHeaders: (headers: Headers, { getState }) => {
         const token = (getState() as RootState).auth.token
