f709f38993c1b0d05141cea55a6b02d7eef9a99f
TsvetomirMichov <94786397+TsvetomirMichov@users.noreply.github.com>
2024-04-11 19:19:05 +0300

    Update Footer.tsx

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,25 +1,45 @@
 import { Divider } from '@mui/material';
-import { useState } from 'react';
+import {  useRef  } from 'react';
 import { FaFire } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-
-
+import emailjs from '@emailjs/browser';
 
 
 const Footer = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
-  const [email, setEmail] = useState('');
+  const form = useRef<HTMLFormElement>(null);
 
-  const handleSubmit = (e: any) => {
+  const sendEmail = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    // Handle email submission here, e.g., send to API or store in state
-    console.log('Email submitted:', email);
-
-    setEmail('');
+    try {
+      await  emailjs
+      .sendForm('service_4ybzx5m', 'template_q33gu09', form.current as HTMLFormElement, {
+        publicKey: 'sreIIaxh0-GlCd_pO',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  
+    } catch (err) {
+      console.log(err)
+    }
   };
+
+  const scrollToHowItWorks = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+
 
   return (
     <footer className='w-full box-border relative bg-[#2F2F2F]'>
@@ -28,13 +48,12 @@ const Footer = () => {
         <div className='bg-white rounded-full  sm:rounded-full md:px-[10em] md:py-[5em] p-[1em]  w-auto  md:w-4/5 shadow-sm shadow-black flex flex-col items-center'>
           <h2 className='text-xl md:text-2xl font-bold text-center mb-4 flex-wrap'> {t("footer.subscribe.title")} </h2>
           <p className='text-md text-gray-700 text-center flex flex-wrap'> {t("footer.subscribe.description")} </p>
-          <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row items-center'>
+          <form ref={form} onSubmit={sendEmail} className='flex flex-col sm:flex-row items-center'>
             <input
               type='email'
+              name="to_email"
               placeholder={t("footer.subscribe.footerSeachPlaceHolder")}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className='border  px-2 py-1 md:px-3  md:py-2 rounded-md mr-2 focus:outline-none'
+              className='border px-2 py-1 md:px-3 md:py-2 rounded-md mr-2 focus:outline-none'
             />
             <button type='submit' className='bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-500'>
               {t("footer.subscribe.button")}
@@ -55,18 +74,16 @@ const Footer = () => {
         {/* Left and right content (replace with your links) */}
         <div className='flex flex-col gap-4 justify-center '>
           <h1 className='text-xl font-semibold text-white '>{t("footer.company.title")}</h1>
-          <Link to={"/"} className='text-md text-white hover:text-orange-400'>{t("footer.company.links.aboutUs")}</Link>
-          <Link to={"/"} className='text-md text-white hover:text-orange-400'>{t("footer.company.links.howItWorks")}</Link>
-          <Link to={"/"} className='text-md text-white hover:text-orange-400'>{t("footer.company.links.services")}</Link>
+          <Link to={"/about"} className='text-md text-white hover:text-orange-400'>{t("footer.company.links.aboutUs")}</Link>
+          <a onClick={() => scrollToHowItWorks("howitworks")} className='text-md text-white hover:text-orange-400'>{t("footer.company.links.services")}</a>
+          <a onClick={() => scrollToHowItWorks("whyourrestaurants")} className='text-md text-white hover:text-orange-400'>{t("footer.company.links.howItWorks")}</a>
         </div>
-
 
         <div className='flex flex-col gap-4 justify-center'>
           <h1 className='text-xl font-semibold text-white '>{t("footer.getInTouch.title")}</h1>
           <p className='text-md text-white hover:text-orange-400'>{t("footer.getInTouch.phone")}</p>
           <p className='text-md text-white hover:text-orange-400'>{t("footer.getInTouch.email")}</p>
         </div>
-
 
       </div>
       <div className='flex flex-col w-full gap-4  items-center py-10  '>
